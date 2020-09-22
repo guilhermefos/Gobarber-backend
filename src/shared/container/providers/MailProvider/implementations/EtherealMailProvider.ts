65fec419cbef0700883159d1b34d96772694548d
Guilherme Ferreira <guilhermefos.developer@gmail.com>
2020-09-22 06:42:21 -0300

    Working in progress

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,10 +1,17 @@
 import nodemailer, { Transporter } from 'nodemailer';
+import { injectable, inject } from 'tsyringe';
 import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
+import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
 
+@injectable()
 export default class FakeMailProvider implements IMailProvider {
     private client: Transporter;
 
-    constructor() {
+    constructor(
+        @inject('MailTemplateProvider')
+        private mailTemplateProvider: IMailTemplateProvider
+    ) {
         this.setupNodeMailer();
     }
 
@@ -23,12 +30,18 @@ export default class FakeMailProvider implements IMailProvider {
         this.client = transporter;
     }
 
-    public async sendMail(to: string, body: string): Promise<void> {
+    public async sendMail({ to, from, subject, templateData }: ISendMailDTO): Promise<void> {
         const message = await this.client.sendMail({
-            from: 'Equipe GoBarber <equipe@gorbarber.com.br>',
-            to,
-            subject: 'Recuperaçào de senha',
-            text: body,
+            from: {
+                name: from?.name || 'Equipe GoBarber',
+                address: from?.email || 'equipe@gobarber.com.br'
+            },
+            to: {
+                name: to.name,
+                address: to.email
+            },
+            subject,
+            html: await this.mailTemplateProvider.parse(templateData),
         });
 
         console.log('Message sent: %s', message.messageId);
