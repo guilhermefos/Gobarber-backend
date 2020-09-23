9864aa927618a6dda37bca6e8846d81bdd0dd383
Guilherme Ferreira <guilhermefos.developer@gmail.com>
2020-09-23 20:28:25 -0300

    Add routes and Controllers to manage day and month availability

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,18 @@ import { Router } from 'express'
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
 
 const router = Router()
 const providersController = new ProvidersController()
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController()
+const providerDayAvailabilityController = new ProviderDayAvailabilityController()
 
 router.use(ensureAuthenticated)
 
 router.get('/', providersController.index);
+router.get('/:provider_id/month-availability', providerMonthAvailabilityController.index);
+router.get('/:provider_id/day-availability', providerDayAvailabilityController.index);
 
 export default router
