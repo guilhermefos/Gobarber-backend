b8868bf45874bcd2764b90b0d884b5eba57b9349
Guilherme Ferreira <guilhermefos.developer@gmail.com>
2020-09-23 20:53:48 -0300

    Make Schedule

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,18 +2,15 @@ import { Router } from 'express'
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 
 import AppointmentController from '@modules/appointments/infra/http/controllers/AppointmentController';
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
 
 const router = Router()
 const appointmentController = new AppointmentController()
+const providerAppointmentsController = new ProviderAppointmentsController()
 
 router.use(ensureAuthenticated)
 
-// router.get('/', async (request, response) => {
-//   const appointments = await repository.find()
-
-//   return response.json(appointments)
-// })
-
 router.post('/', appointmentController.create);
+router.get('/me', providerAppointmentsController.index);
 
 export default router
