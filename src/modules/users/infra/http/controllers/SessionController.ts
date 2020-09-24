d043a8ea502c05d1be2feca209256792f0d8b4eb
Guilherme Ferreira <guilhermefos.developer@gmail.com>
2020-09-24 11:37:38 -0300

    Add Transformer to Responses

--- src/modules/users/infra/http/controllers/SessionController.ts
+++ src/modules/users/infra/http/controllers/SessionController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from "tsyringe";
+import { classToClass } from "class-transformer";
 
 import AuthenticateUserService from '../../../services/AuthenticateUserService'
 
@@ -9,8 +10,6 @@ export default class SessionController {
 
         const { user, token } = await service.execute({ ...request.body })
 
-        delete user.password
-
-        return response.json({ user, token })
+        return response.json({ user: classToClass(user), token })
     }
 }
--- src/modules/users/infra/http/controllers/UserController.ts
+++ src/modules/users/infra/http/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from "tsyringe";
+import { classToClass } from "class-transformer";
 import CreateUserService from '../../../services/CreateUserService'
 
 export default class UserController {
@@ -8,8 +9,6 @@ export default class UserController {
 
         const user = await userService.execute({ ...request.body })
 
-        delete user.password
-
-        return response.json(user)
+        return response.json(classToClass(user))
     }
 }
